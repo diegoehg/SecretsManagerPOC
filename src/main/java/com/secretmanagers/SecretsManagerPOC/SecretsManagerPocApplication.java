eaef034804af1aa2b1da07418dade9885e66dc59
Diego Hernández <diego.hernandez@encora.com>
2024-08-07 13:26:30 -0600

    feat:Initial commit

---
+++ src/main/java/com/secretmanagers/SecretsManagerPOC/SecretsManagerPocApplication.java
@@ -0,0 +1,13 @@
+package com.secretmanagers.SecretsManagerPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretsManagerPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretsManagerPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secretmanagers/SecretsManagerPOC/SecretsManagerPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secretmanagers.SecretsManagerPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretsManagerPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
